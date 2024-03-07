0bcf9e695b8ed4ecf3a88a3ac42750bb4dbeed18
dharaneesh1212 <dharun12122002@gmail.com>
2024-03-07 10:56:27 +0530

    copy

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 `use strict`;
 
+//otp generation
+
 const generateOtp = () => {
+  let otp = Math.floor(100000 + Math.random() * 900000);
 
-    let otp = Math.floor(100000 + Math.random() * 900000);
-
-    document.getElementById("otpInput").innerText = `${otp}`;
+  document.getElementById("otpInput").textContent = `${otp}`;
+  //instead of using innerText use textContent in paragraph element
 };
 document.getElementById("otpButton").addEventListener("click", generateOtp);
+
+//otp copying
+
+const copy = () => {
+  let value = document.getElementById("otpInput").textContent;
+  //if you want to copy the OTP from a paragraph element use textContent
+
+  if (value) {
+    navigator.clipboard
+      .writeText(value)
+      .then(() => {
+        alert("OTP copied to clipboard!");
+      })
+      .catch((error) => {
+        console.log("error to copy", error);
+        alert("Failed to copy");
+      });
+  } else {
+    alert("Generate OTP first before copying.");
+  }
+};
+document.getElementById("copyButton").addEventListener("click", copy);
