71ced9516f97b586b743b3c9db0ba511bee13623
dharaneesh1212 <dharun12122002@gmail.com>
2024-03-07 10:11:00 +0530

    otp complete

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-`use strict`
+`use strict`;
 
+const generateOtp = () => {
 
+    let otp = Math.floor(100000 + Math.random() * 900000);
+
+    document.getElementById("otpInput").innerText = `${otp}`;
+};
+document.getElementById("otpButton").addEventListener("click", generateOtp);
